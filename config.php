eb0bf480410ed03835ea4d579b28b1256f9927f6
sunkarakamal <sunkarakamal@gmail.com>
2024-04-22 13:06:13 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $url='localhost';
 $username='root';
-$password='JT@123';
+$password='Jhaishna@899747';
 $con=mysqli_connect($url,$username,$password,"test");
 if(!$con){
  die('Could not Connect My Sql:' .mysql_error());
