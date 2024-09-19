dcdfc58c90f4847bceba5e83edc75a9ee16561ac
Kamalnadh Sunkara <kamalnadh.sunkara@pearson.com>
2024-09-19 15:03:02 +0100

    updating the folder changes

--- jhaishna/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$url='localhost';
-$username='root';
-$password='Jhaishna@899747';
-$con = mysqli_connect($url, $username, $password, "test");
-if(!$con){
- die('Could not Connect My Sql:' .mysql_error());
-}
-?>
